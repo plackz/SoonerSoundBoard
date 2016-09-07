3e58a60e8b12dac6b2aa01de143f24c55192f512
plackz <pzaffina@slb.com>
2016-09-07 00:27:49 -0500

    initial commit

---
+++ Untitled Folder/main.js
@@ -0,0 +1,51 @@
+/* main.js */
+/* play function. to play music files */
+function play(){
+  var audio = document.getElementById("audio");
+  audio.play();
+}
+
+function pause(){
+  var audio = document.getElementById("audio");
+  audio.pause();
+}
+
+function play1(){
+  var audio1 = document.getElementById("audio1");
+  audio1.play();
+}
+
+function pause1(){
+  var audio1 = document.getElementById("audio1");
+  audio1.pause();
+}
+
+function play2(){
+  var audio2 = document.getElementById("audio2");
+  audio2.play();
+}
+
+function pause2(){
+  var audio2 = document.getElementById("audio2");
+  audio2.pause();
+}
+
+function play3(){
+  var audio3 = document.getElementById("audio3");
+  audio3.play();
+}
+
+function pause3(){
+  var audio3 = document.getElementById("audio3");
+  audio3.pause();
+}
+
+function play4(){
+  var audio4 = document.getElementById("audio4");
+  audio4.play();
+}
+
+function pause4(){
+  var audio4 = document.getElementById("audio4");
+  audio4.pause();
+}
